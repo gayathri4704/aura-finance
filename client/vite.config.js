274f037e54db6b4bc36089a3e9790ae9add0e77d
gayathri4704 <gayathri4704@gmail.com>
2026-03-31 20:48:08 +0530

    Fixed react-is and icon paths for build

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         background_color: '#f8fafc',
         icons: [
           {
-            src: '/icon-192x192.png',
+            src: 'icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icon-512x512.png',
+            src: 'icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
