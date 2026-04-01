0277439255570942794c90bcc024f203d615a765
gayathri4704 <gayathri4704@gmail.com>
2026-04-01 15:27:11 +0530

    Fixed Render ENETUNREACH IPv6 issue

--- server/index.js
+++ server/index.js
@@ -8,15 +8,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 // Email Setup - FIXED FOR RENDER (Connection Timeout Issue)
-const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
+
 
 const app = express();
 app.use(cors());
